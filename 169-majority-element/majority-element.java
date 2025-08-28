d3a17cff97ccea3df1aa666c7849b9841fa28da7
Tharsan S <166637132+Tharsan07@users.noreply.github.com>
2025-08-28 20:15:10 +0530

    Time: 2190 ms (5.04%) | Memory: 53.1 MB (49.73%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,15 +1,40 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        //  int n=nums.length;
+        // int max=n/2;
+        // Map<Integer,Integer>map=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     int num=map.getOrDefault(nums[i],0);
+        //     map.put(nums[i],num+1);
+        // }
+        // for(Map.Entry<Integer, Integer> it : map.entrySet()){
+        //     if(it.getValue()>max){
+        //         return it.getKey();
+        //     }
+        // }
+        // return -1;
+
          int n=nums.length;
-        int max=n/2;
-        Map<Integer,Integer>map=new HashMap<>();
+        int maxOcc= n/2;
+
+        int cnt=0;
+        int max=0;
+        int val=0;
         for(int i=0;i<n;i++){
-            int num=map.getOrDefault(nums[i],0);
-            map.put(nums[i],num+1);
-        }
-        for(Map.Entry<Integer, Integer> it : map.entrySet()){
-            if(it.getValue()>max){
-                return it.getKey();
+            int curr=nums[i];
+            cnt=0;
+            for(int j=0;j<n;j++){
+                if(nums[j]==curr){
+                    cnt++;
+                    if(cnt>max){
+                        max=cnt;
+                        val=curr;
+                        if(max>maxOcc){
+                            // System.out.println(val);
+                            return val;
+                        }
+                    }
+                }
             }
         }
         return -1;
