0b9f4140820af8280176362fee5e7f1ddada2cc9
Tharsan S <166637132+Tharsan07@users.noreply.github.com>
2025-08-31 14:56:51 +0530

    Time: 1 ms (99.54%) | Memory: 45.7 MB (13.89%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int  max=0;
+       int jumps=0;
+       int end=0;
+          for (int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max, i + nums[i]);
+            if (i == end) {
+                jumps++;
+                end = max;
+            }
+        }
+        
+        return jumps;
+    }
+    }
