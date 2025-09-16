210cdb2befdb9cabf4e7b528598fa89fc7083f86
Tharsan S <166637132+Tharsan07@users.noreply.github.com>
2025-09-16 06:24:35 +0530

    Time: 2 ms (77.37%) | Memory: 44.7 MB (84.30%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,20 +1,29 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
        
-       StringBuilder sb=new StringBuilder(magazine);
+    //    StringBuilder sb=new StringBuilder(magazine);
 
-    char[] arr=ransomNote.toCharArray();
-       for(int i=0;i<arr.length;i++){
-        int ind=sb.indexOf(String.valueOf(arr[i]));
-        if(ind==-1){
+    // char[] arr=ransomNote.toCharArray();
+    //    for(int i=0;i<arr.length;i++){
+    //     int ind=sb.indexOf(String.valueOf(arr[i]));
+    //     if(ind==-1){
+    //         return false;
+    //     }
+    //     sb.deleteCharAt(ind);
+    //    }
+    //    return true;
+       
+       int[] occur=new int[26];
+       for(char c: magazine.toCharArray()){
+        occur[c-'a']++;
+       }
+       for(char c:ransomNote.toCharArray()){
+        if(occur[c-'a']==0){
             return false;
         }
-        sb.deleteCharAt(ind);
+        occur[c-'a']--;
        }
        return true;
-       
-       
-       
        
        
        
